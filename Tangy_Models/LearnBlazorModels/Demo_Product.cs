bbcd279e99c0c92d9454788f3497703948aad983
tee2490@hotmail.com <tee2490@hotmail.com>
2023-12-19 18:27:55 +0700

    Section 3 One Way Data Binding

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Tangy_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }       
+    }
+}
