c98ec7d39856cf9739f2c86376619351c8defbcc
tee2490@hotmail.com <tee2490@hotmail.com>
2023-12-20 05:48:53 +0700

    Section 3 Dropdowns and Data Binding

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        public bool IsActive { get; set; }       
+        public bool IsActive { get; set; }
+        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
