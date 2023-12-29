23c28d86c61c18bcac9fe05e4e121ecb5a7eb481
tee2490@hotmail.com <tee2490@hotmail.com>
2023-12-29 10:09:37 +0700

    Section 8 – Setup ApplicationDbContext

---
+++ Tangy_DataAccess/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Tangy_DataAccess
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Tangy_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SqlExpress; Database=TestBlazor67;" +
+                "Trusted_connection=true; TrustServerCertificate=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
