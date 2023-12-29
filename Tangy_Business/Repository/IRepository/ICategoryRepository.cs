12bd814840be8d64f8b96199db9988fa4aac3f55
tee2490@hotmail.com <tee2490@hotmail.com>
2023-12-29 12:48:45 +0700

    Section 8 - Category Repository Setup

---
+++ Tangy_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using Tangy_Models;
+
+namespace Tangy_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO);
+        public CategoryDTO Update(CategoryDTO objDTO);
+        public int Delete(int id);
+        public CategoryDTO Get(int id);
+        public IEnumerable<CategoryDTO> GetAll();
+    }
+}
---
+++ Tangy_Models/CategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Tangy_Models
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
