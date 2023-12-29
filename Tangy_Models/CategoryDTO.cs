37b8d1f4416457599fe39d784a859e4f83b1eb34
tee2490@hotmail.com <tee2490@hotmail.com>
2023-12-29 16:46:15 +0700

    Section 9 - Validations Form & Create Category

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Tangy_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name..")]
         public string Name { get; set; }
     }
 }
