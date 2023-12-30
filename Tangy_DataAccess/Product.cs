e38a646f64e9472e4bb246cbb03b7c300784ce42
tee2490@hotmail.com <tee2490@hotmail.com>
2023-12-30 16:40:59 +0700

    Section 11 - Created Product Table & Product DTO

---
+++ Tangy_DataAccess/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tangy_DataAccess
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool ShopFavorites { get; set; }
+        public bool CustomerFavorites { get; set; }
+        public string Color { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+    }
+}
