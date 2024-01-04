d5f85a3f772bdaf3c18b9fabce2f0672f3e13908
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-04 15:33:26 +0700

    Section 13 – Success and Error DTO

---
+++ Tangy_Models/SuccessModelDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class SuccessModelDTO
+    {
+        public int StatusCode { get; set; }
+        public string SuccessMessage { get; set; }
+        public object Data { get; set; }
+    }
+}
