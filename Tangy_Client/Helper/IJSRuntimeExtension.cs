6a006f23d5dfb2ec6c6babefb6f306f689a6edc2
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-05 11:14:43 +0700

    Section 14 - Client : Basic Settings like Server Project

---
+++ Tangy_Client/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+
+namespace Tangy_Client.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask<bool> ConfirmBox(this IJSRuntime jsRuntime, string message)
+        {
+            return await jsRuntime.InvokeAsync<bool>("confirm", message);
+        }
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ Tangy_Client/wwwroot/js/Common.js
@@ -0,0 +1,35 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 2000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 2000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
+    }
+}
+
+function ShowDeleteConfirmationModal()
+{
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal()
+{
+    $('#deleteConfirmationModal').modal('hide');
+}
