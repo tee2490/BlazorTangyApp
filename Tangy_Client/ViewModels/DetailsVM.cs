bc44736e4aef6c7713abeca2015c8c3395a46b7a
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-05 20:09:52 +0700

    Section 14 – Client : Details VM

---
+++ Tangy_Client/ViewModels/DetailsVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tangy_Models;
+
+namespace Tangy_Client.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            ProductPrice = new();
+            Count = 1;
+        }
+
+        [Required]
+        public int SelectedProductPriceId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value greater than 0")]
+        public int Count { get; set; }
+        
+        public ProductPriceDTO ProductPrice { get; set; }
+    }
+}
