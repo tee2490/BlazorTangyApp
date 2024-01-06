f034a74d9e93dba1fb3e3740e81f606042052516
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-06 09:47:00 +0700

    Section 15 - Client : Implement Cart Service

--- Tangy_Client/Service/CartService.cs
+++ Tangy_Client/Service/CartService.cs
@@ -1,18 +1,72 @@
-﻿using Tangy_Client.Service.IService;
+﻿using Blazored.LocalStorage;
+using Tangy_Client.Service.IService;
 using Tangy_Client.ViewModels;
+using Tangy_Common;
 
 namespace Tangy_Client.Service
 {
     public class CartService : ICartService
     {
-        public Task DecrementCart(ShoppingCart shoppingCart)
+        private readonly ILocalStorageService _localStorage;
+
+        public CartService(ILocalStorageService localStorageService)
         {
-            throw new NotImplementedException();
+            _localStorage = localStorageService;
         }
 
-        public Task IncrementCart(ShoppingCart shoppingCart)
+        public async Task IncrementCart(ShoppingCart shoppingCart)
         {
-            throw new NotImplementedException();
+            var cart = await _localStorage.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
+            bool itemInCart = false;
+
+            if (cart == null)
+            {
+                cart = new List<ShoppingCart>();
+            }
+
+            foreach (var obj in cart)
+            {
+                if (obj.ProductId == shoppingCart.ProductId && obj.ProductPriceId == shoppingCart.ProductPriceId)
+                {
+                    itemInCart = true;
+                    obj.Count += shoppingCart.Count;
+                }
+            }
+
+            if (!itemInCart) //ถ้าเป็น size ที่ยังไม่เคยซื้อ
+            {
+                cart.Add(new ShoppingCart()
+                {
+                    ProductId = shoppingCart.ProductId,
+                    ProductPriceId = shoppingCart.ProductPriceId,
+                    Count = shoppingCart.Count
+                });
+            }
+
+            await _localStorage.SetItemAsync(SD.ShoppingCart, cart);
+        }
+
+        public async Task DecrementCart(ShoppingCart shoppingCart)
+        {
+            var cart = await _localStorage.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
+
+            //if count is 0 or 1 then we remove the item
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].ProductId == shoppingCart.ProductId && cart[i].ProductPriceId == shoppingCart.ProductPriceId)
+                {
+                    if (cart[i].Count == 1 || cart[i].Count == 0)
+                    {
+                        cart.Remove(cart[i]); //ไม่สามารถใช้ foreach เพราะมีการลบที่ต้องระบุตำแหน่งที่ i
+                    }
+                    else
+                    {
+                        cart[i].Count -= shoppingCart.Count;
+                    }
+                }
+            }
+
+            await _localStorage.SetItemAsync(SD.ShoppingCart, cart);
         }
     }
 }
---
+++ Tangy_Common/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace Tangy_Common
+{
+    //สถานะการซื้อสินค้า สำหรับใช้หลายโปรเจค
+    public static class SD
+    {
+        public const string ShoppingCart = "ShoppingCart";
+    }
+}
