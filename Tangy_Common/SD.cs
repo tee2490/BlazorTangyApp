ea7c516047c6e3b3cc7ab77609819c57bef622c4
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-07 14:10:42 +0700

    Section 16 - Order Repository Implementation

--- Tangy_Common/SD.cs
+++ Tangy_Common/SD.cs
@@ -4,5 +4,10 @@
     public static class SD
     {
         public const string ShoppingCart = "ShoppingCart";
+        public const string Status_Pending = "Pending";
+        public const string Status_Confirmed = "Confirmed";
+        public const string Status_Shipped = "Shipped";
+        public const string Status_Refunded = "Refunded";
+        public const string Status_Cancelled = "Cancelled";
     }
 }
