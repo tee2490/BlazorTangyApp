ce9c1e4649bf138089f6eee576081a2663e403a6
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-07 12:35:12 +0700

    Section 16 - Create Order Model and DTO

---
+++ Tangy_Models/OrderDetailDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tangy_Models
+{
+    public class OrderDetailDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+
+        //สินค้าที่สั่งซื้อ
+        [Required]
+        public int ProductId { get; set; }
+        public ProductDTO Product { get; set; }
+
+        //จำนวนและขนาดสินค้าที่สั่งซื้อ
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string Size { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+    }
+}
---
+++ Tangy_Models/OrderHeaderDTO.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tangy_Models
+{
+    public class OrderHeaderDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        //ส่วนสรุปการสั่งซื้อสินค้า
+        [Required]
+        [Display(Name = "Order Total")]
+        public double OrderTotal { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        [Display(Name = "Shipping Date")]
+        public DateTime ShippingDate { get; set; }
+        [Required]
+        public string Status { get; set; }
+
+        //stripe payment รหัสและสถานะการชำระเงิน
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+
+        //ข้อมูลทั่วไปของลูกค้า
+        [Display(Name = "Name")]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+    }
+}
