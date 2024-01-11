7c912a62d72b3b55dc40d418917b87cd237445f1
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-11 17:26:56 +0700

    Section 18 - Add Identity to API

--- Tangy_API/Program.cs
+++ Tangy_API/Program.cs
@@ -1,10 +1,22 @@
 global using Tangy_Business.Repository;
 global using Tangy_Business.Repository.IRepository;
 global using Tangy_DataAccess.Data;
+global using Tangy_DataAccess;
+using Microsoft.AspNetCore.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+}).AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
