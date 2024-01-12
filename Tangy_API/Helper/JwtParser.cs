754dd25a7291faf9e2074240c1f8ed30137195fe
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-12 15:59:02 +0700

    Section 19 - Client : JwtParser

---
+++ Tangy_API/Helper/JwtParser.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace Tangy_API.Helper
+{
+    public static class JwtParser
+    {
+        //แปลงจาก Token เป็นค่าที่อ่านได้ประกอบด้วย key กับ value
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var claims = new List<Claim>();
+            var payload = jwt.Split('.')[1];
+
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+            return claims;
+        }
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
