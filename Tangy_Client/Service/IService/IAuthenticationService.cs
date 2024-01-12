541114204ba145c9e05a66bca3fda9e7028c8545
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-12 17:28:25 +0700

    Section 19 - Client : IauthenticationService

---
+++ Tangy_Client/Service/IService/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Tangy_Models;
+
+namespace Tangy_Client.Service.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequestDTO);
+        Task<SignInResponseDTO> Login(SignInRequestDTO signInRequestDTO);
+        Task Logout();
+    }
+}
