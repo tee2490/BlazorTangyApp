08012d30b362ab8998297a942db532de77bb2d47
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-17 15:28:12 +0700

    Section 20 - Order Confirmation

--- Tangy_Common/SD.cs
+++ Tangy_Common/SD.cs
@@ -15,5 +15,6 @@
 
         public const string Local_Token = "JWT Token";
         public const string Local_UserDetails = "UserDetails";
+        public const string Local_OrderDetails = "Local_OrderDetails";
     }
 }
