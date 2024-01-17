15874f18b84626bf865a6b8cd67aa0bfb549e8dd
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-17 10:00:59 +0700

    Section 20 – API : Stripe Payment Controller Method

---
+++ Tangy_API/Controllers/StripePaymentController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+using Tangy_Models;
+
+namespace Tangy_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StripePaymentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public StripePaymentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost, Authorize]
+        [ActionName("Create")]
+        public async Task<IActionResult> Create([FromBody] StripePaymentDTO paymentDTO)
+        {
+            try
+            {
+                var domain = _configuration.GetValue<string>("Tangy_Client_URL");
+
+                //session stripe .net : https://stripe.com/docs/api/checkout/sessions/create?lang=dotnet
+                //Stripe-hosted page : https://stripe.com/docs/checkout/quickstart?lang=dotnet
+
+                //สร้างข้อมูลตามแบบฟอร์มของ stripe 
+                var options = new SessionCreateOptions
+                {
+                    SuccessUrl = domain + paymentDTO.SuccessUrl,
+                    CancelUrl = domain + paymentDTO.CancelUrl,
+                    LineItems = new List<SessionLineItemOptions>(),
+                    Mode = "payment",
+                    PaymentMethodTypes = new List<string> { "card" }
+                };
+
+                foreach (var item in paymentDTO.Order.OrderDetails)
+                {
+                    var sessionLineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100), //20.00 -> 2000
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Product.Name
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItem);
+                }
+
+                var service = new SessionService(); //สร้างออบเจคสำหรับขอใช้บริการของ stripe   
+                Session session = service.Create(options); //ส่งไปยัง stripe API
+
+                return Ok(new SuccessModelDTO()
+                {
+                    Data = session.Id
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ErrorModelDTO()
+                {
+                    ErrorMessage = ex.Message
+                });
+            }
+        }
+    }
+}
--- Tangy_Models/StripePaymentDTO.cs
+++ Tangy_Models/StripePaymentDTO.cs
@@ -2,6 +2,16 @@
 {
     public class StripePaymentDTO
     {
+        public StripePaymentDTO()
+        {
+            SuccessUrl = "OrderConfirmation";
+            CancelUrl = "Summary";
+        }
+
         public OrderDTO Order { get; set; }
+
+        //SuccessUrl และ CancelUrl ใช้แนบข้อความให้กับ url สำหรับ redirect ผลลัพธ์สำเร็จหรือไม่
+        public string SuccessUrl { get; set; }
+        public string CancelUrl { get; set; }
     }
 }
