80b8991929f03722c44ad6aeab34754e1734da61
tee2490@hotmail.com <tee2490@hotmail.com>
2024-01-18 19:37:24 +0700

    Section 21 - Server : Order Details Component

--- Tangy_DataAccess/OrderHeader.cs
+++ Tangy_DataAccess/OrderHeader.cs
@@ -40,5 +40,7 @@ namespace Tangy_DataAccess
         public string PostalCode { get; set; }
         [Required]
         public string Email { get; set; }
+        public string? Tracking { get; set; }
+        public string? Carrier { get; set; }
     }
 }
